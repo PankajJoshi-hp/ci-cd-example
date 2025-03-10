5ba7df755165d511c2e54b62c0a91ca0c94532c7
Pankaj Joshi <Pankaj.Joshi.hp@gmail.com>
2025-03-10 19:21:22 +0530

    changed flutter.yml file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,10 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -7,12 +14,6 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-
-    // Ensure all animations and async work are completed.
-    await tester.pumpAndSettle();
-
-    // Debugging: Print all text widgets to see if '0' exists.
-    debugPrint(tester.widgetList(find.byType(Text)).toString());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
